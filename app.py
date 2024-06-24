a55279b82453129078a0aed6a6a9427c43cf6663
bfc07 <bofanc07@outlook.com>
2024-06-24 21:16:28 +0800

    flaskapp

---
+++ app.py
@@ -0,0 +1,97 @@
+import identity.web
+import requests
+from flask import Flask, redirect, render_template, request, session, url_for, flash
+from flask_session import Session
+
+import app_config
+
+# connection to database
+from models import db, User
+from sqlalchemy.exc import IntegrityError
+
+
+
+app = Flask(__name__)
+app.config.from_object(app_config)
+assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
+Session(app)
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# This section is needed for url_for("foo", _external=True) to automatically
+# generate http scheme when this sample is running on localhost,
+# and to generate https scheme when it is deployed behind reversed proxy.
+# See also https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", **auth.log_in(
+        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
+        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
+        prompt="select_account",  # Optional. More values defined in  https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+    ))
+
+
+@app.route(app_config.REDIRECT_PATH)
+def auth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return render_template("auth_error.html", result=result)
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    return redirect(auth.log_out(url_for("index", _external=True)))
+
+
+@app.get("/")
+def index():
+    if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+        # This check is not strictly necessary.
+        # You can remove this check from your production code.
+        return render_template('config_error.html')
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    return render_template('index.html', user=auth.get_user())
+
+
+@app.post("/")
+def handle_submit():
+    username = auth.get_user().get("name")
+    host = request.form.get("host")
+    category = request.form.get("category")
+
+    user = User(
+        username=username,
+        host=host,
+        category=category
+    )
+    
+    try:
+        db.session.add(user)
+        db.session.commit()
+        flash("Submitted", "success")
+    except IntegrityError:      # if a unique value is duplicated
+        flash("Host already exists.", "error")
+
+    return redirect(url_for("index"))
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+  id: Mapped[int] = mapped_column(primary_key=True)
+  username: Mapped[str] = mapped_column(unique=False)
+  host: Mapped[str] = mapped_column(unique=True)
+  category: Mapped[str] = mapped_column(unique=False)
